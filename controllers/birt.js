d368493f1f4aaf342c70a4e06d89310db9cf2913
Maciej Krzysiak <krzysiak.maciek@gmail.com>
2015-11-27 13:26:29 +0100

    fast

--- controllers/birt.js
+++ controllers/birt.js
@@ -1,7 +1,38 @@
-genReport(res, quety){
+var os = require('os');
+var childProcess = require('child_process');
+var execOptions = {maxBuffer: 100*1024, encoding:'utf8', timeout:5000};
+var reportTemp = 'hello_world.rptdesign'
+
+function linuxGenReport(res, query){
+    console.log(query);
+    var child = childProcess.execFile('./ReportEngine/genReport.sh',
+     ['-f', 'PDF', './ReportTemp/'+reportTemp],
+     execOptions, function (err, stdout, stderr){
+       if (err){
+         console.log(err.stack);
+       }
+     });
+     child.on('exit',function(){
+       console.log('Generate report.');
+     });
+}
+
+function winGenReport(res, query){
+
+}
+
+function genReport(res, query){
+  switch (os.platform()[0]){
+    case 'l':
+    linuxGenReport(res, query);
+    break;
+    case 'w':
+    winGenReport(res, query);
+    break;
+  }
 }
 var Birt = {
-  function: go(res, action, query){
+  go: function(res, action, query){
     switch (action){
       case 'genReport':
       genReport(res, query);
@@ -9,4 +40,4 @@ var Birt = {
     }
   }
 }
-module.exports = BirtService;
+module.exports = Birt;
--- routes.js
+++ routes.js
@@ -4,10 +4,8 @@ Routes = {
     var surl = url.parse(req.url, true, false);
     var path = surl.pathname.split('/');
     path.shift();
-    console.log(path);
-    console.log(surl.query);
     var controller = require('./controllers/'+path[0]+'.js')
-    controller.go(req, res, path[1], surl.query);
+    controller.go(res, path[1], surl.query);
   }
 }
 module.exports = Routes;
